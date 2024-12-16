cd09dad774c2126cb3c7a324b4ad65a4cd899289
Danishwara Pracheta <danishwara81@gmail.com>
2024-12-16 19:50:02 +0800

    added file sorting

--- backend/handler/sortFile.js
+++ backend/handler/sortFile.js
@@ -26,4 +26,41 @@ async function handleFileList(req, res) {
     }
 }
 
-module.exports = { handleFileList };
+// Sort by file name
+async function sort_by_name(arr) {
+    arr.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        else if (a.name > b.name) return 1;
+    });
+}
+
+
+// Sort by file last modified date
+async function sort_by_date(arr) {
+    arr.sort((a, b) => {
+        if (a.date < b.date) return -1;
+        else if (a.date > b.date) return 1;
+
+        else if (a.name < b.name) return -1;
+        else if (a.name > b.name) return 1;
+    });
+}
+
+// Sort by file size
+async function sort_by_size(arr) {
+    arr.sort((a, b) => {
+        if (a.size < b.size) return -1;
+        if (a.size > b.size) return 1;
+
+        else if (a.name < b.name) return -1;
+        else if (a.name > b.name) return 1;
+    });
+}
+
+
+module.exports = { 
+    handleFileList,
+    sort_by_name,
+    sort_by_date,
+    sort_by_size,
+};
